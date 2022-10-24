55c79eab9d421a344df7176c3004b0d7a568ef3e
itsArjae <arjaeiporong@gmail.com>
2022-10-25 03:53:25 +0800

    kitchen

--- pages/kitchen/kitchen.home.js
+++ pages/kitchen/kitchen.home.js
@@ -1,13 +1,146 @@
-import React from 'react'
-import styles from '../../styles/css/kitchen-styles/kitchen.home.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/css/kitchen-styles/kitchen.home.module.css";
+import {
+  collection,
+  getFirestore,
+  query,
+  onSnapshot,
+  where,
+  orderBy,
+  limitToLast,
+} from "firebase/firestore";
+import {app} from '../../src/utility/firebase'
+import { Divider } from "@mui/material";
+import ReactPaginate from "react-paginate";
 
 export default function KitchenHome() {
+  const [orderQueue, setOrderQueue] = useState([]);
+  const db = getFirestore(app)
+  const getOrderQueue= () => {
+    const orderRef = collection(db, "orderQueue");
+    console.log("read queue");
+    const q = query(orderRef, orderBy("timeStamp","asc"));
+    onSnapshot(q, (snapshot) => {
+      let order = [];
+      snapshot.docs.forEach((doc) => {
+        order.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(order);
+
+      setOrderQueue(order);
+    //  setDailySales(sale);
+    });
+  };
+
+  useEffect(()=>{
+    getOrderQueue();
+  },[]);
 
   return (
-    <div className={styles.container} >
-        <div>
-          <div className={styles.table__list}></div>
+    <div className={styles.container}>
+      <h1>ORDERS</h1>
+      <div className={styles.table__list}>
+
+          {
+            orderQueue.map((data)=>{
+              return(
+                <div className={data.id} >
+                      <OrderBox data={data}  />
+               </div>
+              )
+            })
+          }
+
+      </div>
+    </div>
+  );
+}
+
+
+const OrderBox = (props) => {
+  const db = getFirestore(app)
+  const {data} = props;
+  const [pageNumber,setPageNumber] = useState(1);
+  const itemsPerPage = 2;
+ const [pagesVisited,setPagesVisited] = useState(0);
+ 
+  const [order,setOrder] = useState([]);
+  // .slice(pagesVisited, pagesVisited + itemsPerPage)
+  const displayItems = order
+  .map((item)=>{
+    return(
+      <div key={item.id} >
+       <OrderDetails item={item}/>
+      </div>
+    )
+  })
+
+  const getOrder = () => {
+    const orderRef = collection(db, "orders");
+    console.log("read queue");
+    const q = query(orderRef,where("queueID","==",data.id));
+    onSnapshot(q, (snapshot) => {
+      let order = [];
+      snapshot.docs.forEach((doc) => {
+        order.push({ ...doc.data(), id: doc.id });
+      });
+      //console.log(order);
+      setOrder(order);
+      //setDailySales(sale);
+    });
+  };
+
+  useEffect(()=>{
+    getOrder();
+  },[])
+  const pageCount = Math.ceil(order.length / itemsPerPage);
+  const [doneCooking,setDoneCooking] = useState(false);
+
+  const handleCook = () => {
+    setDoneCooking(true);
+  }
+  return(
+    <div className={styles.q__box} >
+        <div className={styles.q__header} >
+            Table {data.TableId}
         </div>
+        <div className={styles.q__order} >
+          {displayItems}
+        </div>
+        <div className={styles.q__btn} >
+          {
+            doneCooking? <div className={styles.stat__cook} ><div>COOKING</div> <button >SERVE</button> </div>: 
+            <div className={styles.stat__cook1} ><button onClick={handleCook} >COOKING</button> <div>SERVE</div> </div>
+          }
+          {/* <button  >COOKING</button>
+          <button >SERVE</button> */}
+      
+        </div> 
+    
     </div>
   )
+}
+
+
+const OrderDetails = (props) => {
+  const [isSelected,setIsSelected] = useState(false);
+  const {item} = props;
+
+  const handleSelect = () => {
+      setIsSelected(true);
+
+  }
+  return(
+    <div className={styles.order__box} >
+      <div className={styles.order__det} >
+        <div className={styles.itemname} >{item.itemName}</div>
+        <div className={styles.itemqty} >QTY: {item.quantity}</div>
+      </div>
+      <div className={styles.orderbtn} >
+        {
+          isSelected? '✔' : <button onClick={handleSelect} >✔️</button>
+        }
+      </div>
+    </div>
+  );
 }
---
+++ pages/kitchen/kitchen.orderstatus.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/css/kitchen-styles/kitchen.status.module.css";
+import {
+  collection,
+  getFirestore,
+  query,
+  onSnapshot,
+  where,
+  orderBy,
+  limitToLast,
+} from "firebase/firestore";
+import {app} from '../../src/utility/firebase'
+import { data } from "autoprefixer";
+export default function KitchenOrderStatus() {
+  const [preparing,setPreparing] = useState([]);
+  const [cooking,setCooking] = useState([]);
+  const [serving,setServing] = useState([]);
+
+  const db = getFirestore(app);
+  const getPreparingQueue = () => {
+    const orderRef = collection(db, "orderQueue");
+    console.log("read queue");
+    const q = query(orderRef,where("status","==","preparing"),orderBy("timeStamp","asc"));
+    onSnapshot(q, (snapshot) => {
+      let order = [];
+      snapshot.docs.forEach((doc) => {
+        order.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(order);
+
+      setPreparing(order);
+    //  setDailySales(sale);
+    });
+  };
+
+  useEffect(()=>{
+    getPreparingQueue();
+  },[]);
+
+  
+  
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.status__box}>
+          <div className={styles.status__header}>
+           <h2>PREPARING</h2>
+          </div>
+          <div className={styles.list__box} >
+            
+            {
+              preparing.map((data)=>{
+                return(
+                  <div key={data.id} >
+                        <TableInfo data={data}/>
+                    </div>
+                )
+              })
+            }
+           
+
+          </div>
+
+        </div>
+        <div className={styles.status__box}>
+          <div className={styles.status__header1}>
+          <h2>COOKING</h2>
+          </div>
+        </div>
+        <div className={styles.status__box}>
+          <div  className={styles.status__header2}>
+          <h2>SERVING</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const TableInfo = (props) => {
+  const {data} = props;
+  return(
+    <div className={styles.info__box} >
+        Table
+        <div>{data.TableId}</div>
+        </div>
+  )
+}
+
+// style={{backgroundColor:"green"}}
