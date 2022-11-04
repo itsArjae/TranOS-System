130282a37396afb7d743c69d84adadd52752f8ec
itsArjae <arjaeiporong@gmail.com>
2022-11-05 00:39:00 +0800

    first login

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -36,8 +36,8 @@ export default function ResetPassword() {
         <div className={styles.image} >
         <img src="/assets/admin-assets/pictures/logo.png" />
         </div>
-      <h1>THANK YOU THE RESET PASSWORD LINK HAS BEEN SENT TO YOUR EMAIL</h1>
-      <p>IF BY CHANCE THAT YOU DIDN'T RECEIVE THE EMAIL KINDLY REPORT TO THE ADMIN</p>
+      <h1>THANK YOU! THE RESET PASSWORD LINK HAS BEEN SENT TO YOUR EMAIL</h1>
+      <p>IF BY CHANCE THAT YOU DIDN&lsquo;T RECEIVE THE EMAIL KINDLY REPORT TO THE ADMIN</p>
     <button onClick={()=>{router.push('/')}} >GO BACK LOGIN</button>
     </div>
   )
