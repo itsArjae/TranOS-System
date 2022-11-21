5c1e2d15b6c2f91984f1029b5c736c76a27e7a41
itsArjae <arjaeiporong@gmail.com>
2022-11-21 21:49:38 +0800

    update

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -296,11 +296,11 @@ export default function SignIn() {
               width={30}
               height={30}
             />
-            <a href="/app/Tranos_version1.0.5.apk" download>
+            <a href="/app/Tranos_version1.0.8.apk" download>
               <button>Download App Here</button>
             </a>
           </div>
-          Tranos v1.0.5
+          Tranos v1.0.8
         </div>
       </div>
     </div>
